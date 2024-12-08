a0029c7bb5cc0ffe2d36f13318e08d942e034be4
Henok Solomon <henoksolomongiz@gmail.com>
2024-12-08 19:28:56 +0300

    axios and toast dependencies added

--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,16 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import ToastPlugin from 'vue-toast-notification';
 
+import "@/index.css"
 import App from './App.vue'
 import router from './router'
 
 const app = createApp(App)
 
 app.use(createPinia())
+app.use(ToastPlugin)
 app.use(router)
 
 app.mount('#app')
